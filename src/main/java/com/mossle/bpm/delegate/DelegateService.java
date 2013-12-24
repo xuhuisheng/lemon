f3b7aed478a60e0590176c6b065b006f848620c4
Xu Huisheng <xyz20003@gmail.com>
2013-12-24 22:15:19 +0800

    #32 修复自动委托功能

--- src/main/java/com/mossle/bpm/delegate/DelegateService.java
+++ src/main/java/com/mossle/bpm/delegate/DelegateService.java
@@ -12,7 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Component
 public class DelegateService {
     private static Logger logger = LoggerFactory
@@ -84,7 +86,7 @@ public class DelegateService {
         Date now = new Date();
 
         if (startTime != null) {
-            return now.after(startTime);
+            return now.before(startTime);
         }
 
         if (endTime != null) {
