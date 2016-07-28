d1139ce579d6ceb42f2c62a62086b5c059cd9ef1
Xu Huisheng <xyz20003@gmail.com>
2016-07-28 20:36:51 +0800

    解决functionListener在ActivityEnd事件无法获得当前execution的问题

--- src/main/java/com/mossle/bpm/cmd/CompleteTaskWithCommentCmd.java
+++ src/main/java/com/mossle/bpm/cmd/CompleteTaskWithCommentCmd.java
@@ -2,13 +2,17 @@ package com.mossle.bpm.cmd;
 
 import java.util.Map;
 
+import org.activiti.engine.ActivitiException;
 import org.activiti.engine.delegate.TaskListener;
+import org.activiti.engine.delegate.event.ActivitiEventType;
+import org.activiti.engine.delegate.event.impl.ActivitiEventBuilder;
 import org.activiti.engine.impl.context.Context;
 import org.activiti.engine.impl.identity.Authentication;
 import org.activiti.engine.impl.interceptor.Command;
 import org.activiti.engine.impl.interceptor.CommandContext;
 import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
 import org.activiti.engine.impl.persistence.entity.TaskEntity;
+import org.activiti.engine.task.DelegationState;
 import org.activiti.engine.task.IdentityLinkType;
 
 public class CompleteTaskWithCommentCmd implements Command<Object> {
@@ -31,6 +35,15 @@ public class CompleteTaskWithCommentCmd implements Command<Object> {
             taskEntity.setExecutionVariables(variables);
         }
 
+        boolean localScope = false;
+
+        if ((taskEntity.getDelegationState() != null)
+                && taskEntity.getDelegationState().equals(
+                        DelegationState.PENDING)) {
+            throw new ActivitiException(
+                    "A delegated task cannot be completed, but should be resolved instead.");
+        }
+
         taskEntity.fireEvent(TaskListener.EVENTNAME_COMPLETE);
 
         if ((Authentication.getAuthenticatedUserId() != null)
@@ -40,13 +53,30 @@ public class CompleteTaskWithCommentCmd implements Command<Object> {
                     IdentityLinkType.PARTICIPANT);
         }
 
+        if (Context.getProcessEngineConfiguration().getEventDispatcher()
+                .isEnabled()) {
+            Context.getProcessEngineConfiguration()
+                    .getEventDispatcher()
+                    .dispatchEvent(
+                            ActivitiEventBuilder
+                                    .createEntityWithVariablesEvent(
+                                            ActivitiEventType.TASK_COMPLETED,
+                                            taskEntity, variables, localScope));
+        }
+
         Context.getCommandContext().getTaskEntityManager()
                 .deleteTask(taskEntity, comment, false);
 
         if (taskEntity.getExecutionId() != null) {
             ExecutionEntity execution = taskEntity.getExecution();
             execution.removeTask(taskEntity);
-            execution.signal(null, null);
+
+            try {
+                Context.setExecutionContext(execution);
+                execution.signal(null, null);
+            } finally {
+                Context.removeExecutionContext();
+            }
         }
 
         return null;
